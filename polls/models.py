938e663989091b61b3a74c4b7e11ae1d5d38b3b3
ronny karani <karanironny25@gmail.com>
2021-09-01 10:33:26 +0300

    customize admin

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     
     def __str__(self):
         return self.question_text
